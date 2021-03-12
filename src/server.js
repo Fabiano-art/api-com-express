749e42f7c041b431bda04db36337217ab24b7a14
Fabiano Guimarães <fabio.dub@outlook.com>
2021-03-12 12:33:33 -0300

    Primeiro metodo GET

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const port = 3003;
+const express = require('express');
+const app = express();
+
+app.get('/produtos', (req, res, next) => {
+    res.send({nome: "Notebook", preco: 123.45});
+})
+app.listen(port, () => { console.log(`Server is running on port ${port}`) });
